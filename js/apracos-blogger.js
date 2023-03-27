a8f8221ce9f1cedcabe10f563f49d9f74931a3c6
Oleg <82386871+a374ru@users.noreply.github.com>
2023-03-27 05:45:37 +0300

    111

--- js/apracos-blogger.js
+++ js/apracos-blogger.js
@@ -5,15 +5,7 @@ var OLY = (function () {
     this.theMomentTime = new Date();
     this.anchorElemID = "#11";
     this.stateModalView = false;
-    this.arrayDaysRu = [
-      "ВОСРЕСЕНЬЕ",
-      "ПОНЕДЕЛЬНИК",
-      "ВТОРНИК",
-      "СРЕДА",
-      "ЧЕТВЕРГ",
-      "ПЯТНИЦА",
-      "СУББОТА",
-    ];
+    this.arrayDaysRu = ["ВОСРЕСЕНЬЕ", "ПОНЕДЕЛЬНИК", "ВТОРНИК", "СРЕДА", "ЧЕТВЕРГ", "ПЯТНИЦА", "СУББОТА",];
     this.weeks = {};
     this.easterDates = {
       2015: [3, 12],
@@ -174,116 +166,68 @@ var OLY = (function () {
   }
   OLY.prototype.initOLY = function () {
     var yearNumber = this.theMomentTime.getFullYear();
-    if (this.theMomentTime >=
-      new Date(this.theMomentTime.getFullYear(), this.easterDates[yearNumber][0], this.easterDates[yearNumber][1])) {
+    if (this.theMomentTime >= new Date(this.theMomentTime.getFullYear(), this.easterDates[yearNumber][0], this.easterDates[yearNumber][1])) {
       this.oldEaster = new Date(this.theMomentTime.getFullYear(), this.easterDates[yearNumber][0], this.easterDates[yearNumber][1]);
       this.newEaster = new Date(this.theMomentTime.getFullYear() + 1, this.easterDates[yearNumber + 1][0], this.easterDates[yearNumber + 1][1]);
-    }
-    else {
+    } else {
       this.oldEaster = new Date(this.theMomentTime.getFullYear() - 1, this.easterDates[yearNumber - 1][0], this.easterDates[yearNumber - 1][1]);
       this.newEaster = new Date(this.theMomentTime.getFullYear(), this.easterDates[yearNumber][0], this.easterDates[yearNumber][1]);
     }
-    console.log("\n" +
-      "Прошедшая Пасха: " +
-      this.oldEaster.toString().slice(0, 15) +
-      "\n" +
-      "Ожидаемая Пасха: " +
-      this.newEaster.toString().slice(0, 16));
+    console.log("\n" + "Прошедшая Пасха: " + this.oldEaster.toString().slice(0, 15) + "\n" + "Ожидаемая Пасха: " + this.newEaster.toString().slice(0, 16));
     this.oldEasterMLS = this.oldEaster.getTime();
     this.newEasterMLS = this.newEaster.getTime();
     return true;
-  };
+  }
+    ;
   OLY.prototype.initWeeks = function () {
-    var day = (this.weeks["day"] = [
-      this.theMomentTime.getDay() + 1,
-      "День седмицы",
-    ]);
-    var all = (this.weeks["all"] = [
-      Math.ceil((this.newEasterMLS - this.oldEasterMLS) / 864e5 / 7),
-      "Протяженность ПБГ",
-    ]);
-    var current = (this.weeks["current"] = [
-      Math.ceil((this.theMomentTime.getTime() - this.oldEasterMLS) / 864e5 / 7 + 0.1),
-      "Текущая седмица",
-    ]);
+    var day = (this.weeks["day"] = [this.theMomentTime.getDay() + 1, "День седмицы",]);
+    var all = (this.weeks["all"] = [Math.ceil((this.newEasterMLS - this.oldEasterMLS) / 864e5 / 7), "Протяженность ПБГ",]);
+    var current = (this.weeks["current"] = [Math.ceil((this.theMomentTime.getTime() - this.oldEasterMLS) / 864e5 / 7 + 0.1), "Текущая седмица",]);
     var mif = (this.weeks["mif"] = [all[0] - 10, "Седмица МиФ"]);
     var zakhey = (this.weeks["zakhey"] = [mif[0] - 1, "Седмица Закхея"]);
-    var stupkaK = (this.weeks["stupkaK"] = [
-      all[0] - 50,
-      "Крещенская отступка",
-    ]);
-    var vozdvizgenie = (this.weeks["vozdvizgenie"] = [
-      Math.ceil((this.datesOLY.vozdvizgenieKresta[0].getTime() - this.oldEasterMLS) /
-        864e5 /
-        7),
-      "Седмица Воздвижения",
-    ]);
-    var stupkaV = (this.weeks["stupkaV"] = [
-      Math.ceil((this.datesOLY.week24[0].getTime() - this.oldEasterMLS) / 864e5 / 7) - vozdvizgenie[0],
-      "Воздвиженская преступка",
-    ]);
+    var stupkaK = (this.weeks["stupkaK"] = [all[0] - 50, "Крещенская отступка",]);
+    var vozdvizgenie = (this.weeks["vozdvizgenie"] = [Math.ceil((this.datesOLY.vozdvizgenieKresta[0].getTime() - this.oldEasterMLS) / 864e5 / 7), "Седмица Воздвижения",]);
+    var stupkaV = (this.weeks["stupkaV"] = [Math.ceil((this.datesOLY.week24[0].getTime() - this.oldEasterMLS) / 864e5 / 7) - vozdvizgenie[0], "Воздвиженская преступка",]);
     return this.weeks;
-  };
+  }
+    ;
   OLY.prototype.mondayAfterVozdvizgenie = function () {
     var mondayPoVozdvizgene = 1 + 7 - (this.datesOLY.vozdvizgenieKresta[0].getDay() % 7);
     var updateTheDate = this.datesOLY.vozdvizgenieKresta[0].getDate() + mondayPoVozdvizgene;
     this.datesOLY.vozdvizgenieKresta[0].setDate(updateTheDate);
     this.datesOLY.vozdvizgenieKresta[1] = "Понедельник по Воздвижении";
     return this.theMomentTime >= this.datesOLY.vozdvizgenieKresta[0];
-  };
+  }
+    ;
   OLY.prototype.initDatesOLY = function () {
-    this.datesOLY["pentecost"] = [
-      new Date(this.oldEasterMLS + 864e5 * 49),
-      "Пятьдесятница",
-    ];
-    this.datesOLY["vozdvizgenieKresta"] = [
-      new Date(this.oldEaster.getFullYear() + "-09-27T00:00:00"),
-      "Воздвижение Креста Господня",
-    ];
-    this.datesOLY["zakhey"] = [
-      new Date(this.newEasterMLS - 864e5 * 77),
-      "Неделя Закхея",
-    ];
-    this.datesOLY["mif"] = [
-      new Date(this.newEasterMLS - 864e5 * 70),
-      "Неделя МиФ",
-    ];
-    this.datesOLY["aboutTheProdigalSon"] = [
-      new Date(this.newEasterMLS - 864e5 * 63),
-      "Неделя о блудном сыне",
-    ];
-    this.datesOLY["aboutTheLastJudgment"] = [
-      new Date(this.newEasterMLS - 864e5 * 56),
-      "Неделя о страшном суде",
-    ];
-    this.datesOLY["week24"] = [
-      new Date(this.oldEasterMLS + 864e5 * 168),
-      "17 седмица по Пятьдесятнице",
-    ];
+    this.datesOLY["pentecost"] = [new Date(this.oldEasterMLS + 864e5 * 49), "Пятьдесятница",];
+    this.datesOLY["vozdvizgenieKresta"] = [new Date(this.oldEaster.getFullYear() + "-09-27T00:00:00"), "Воздвижение Креста Господня",];
+    this.datesOLY["zakhey"] = [new Date(this.newEasterMLS - 864e5 * 77), "Неделя Закхея",];
+    this.datesOLY["mif"] = [new Date(this.newEasterMLS - 864e5 * 70), "Неделя МиФ",];
+    this.datesOLY["aboutTheProdigalSon"] = [new Date(this.newEasterMLS - 864e5 * 63), "Неделя о блудном сыне",];
+    this.datesOLY["aboutTheLastJudgment"] = [new Date(this.newEasterMLS - 864e5 * 56), "Неделя о страшном суде",];
+    this.datesOLY["week24"] = [new Date(this.oldEasterMLS + 864e5 * 168), "17 седмица по Пятьдесятнице",];
     return this.datesOLY;
-  };
+  }
+    ;
   OLY.prototype.controlDates = function (userYear) {
     var _a, _b;
     var currentDate = this.theMomentTime;
-    var sStorageDate = sessionStorage.getItem('userDate');
-    if (sessionStorage.userDate != null && userYear == undefined) {
-      currentDate = new Date(String(sStorageDate));
-    }
-    else if (userYear != undefined && userYear[0] < 2100 && userYear[0] > 2016) {
+    var uD = sessionStorage.getItem('userDate');
+    if (sessionStorage.userDate != null) {
+      currentDate = new Date(String(uD));
+    } else if (userYear != undefined && userYear[0] < 2034 && userYear[0] > 2016) {
       currentDate = new Date(userYear[0], (_a = userYear[1]) !== null && _a !== void 0 ? _a : currentDate.getMonth(), Number((_b = userYear[2]) !== null && _b !== void 0 ? _b : currentDate.getDate()));
       sessionStorage.setItem('userDate', String(currentDate));
-    }
-    else {
-      console.warn((userYear
-        ? "Год введенный пользователем не подходит (2016-2033)"
-        : "Год пользователем не предоставлен") + ".\n\u0411\u0443\u0434\u0435\u0442 \u0438\u0441\u043F\u043E\u043B\u044C\u0437\u043E\u0432\u0430\u043D \u0442\u0435\u043A\u0443\u0449\u0438\u0439 \u0433\u043E\u0434.\n\u0421\u041F\u0410\u0421\u0418\u0411\u041E \u0417\u0410 \u0412\u041D\u0418\u041C\u0410\u041D\u0418\u0415!");
+    } else {
+      console.warn((userYear ? "Год введенный пользователем не подходит (2016-2033)" : "Год пользователем не предоставлен") + ".\n\u0411\u0443\u0434\u0435\u0442 \u0438\u0441\u043F\u043E\u043B\u044C\u0437\u043E\u0432\u0430\u043D \u0442\u0435\u043A\u0443\u0449\u0438\u0439 \u0433\u043E\u0434.\n\u0421\u041F\u0410\u0421\u0418\u0411\u041E \u0417\u0410 \u0412\u041D\u0418\u041C\u0410\u041D\u0418\u0415!");
     }
     if (currentDate.getFullYear() != this.theMomentTime.getFullYear()) {
-      document.querySelector('body').innerHTML +=
-        "<div class='userdate'><a id='a-visited-userdate' href=\"#\" onclick=\"apr.deleteUserDateFromSessionStorage()\">" + currentDate.toLocaleDateString() + "</a></div>";
+      document.querySelector('body').innerHTML += "<div class='userdate'><a id='a-visited-userdate' href=\"#\" onclick=\"sessionStorage.removeItem('userDate')\">" + currentDate.toLocaleDateString() + "</a></div>";
     }
     return currentDate;
-  };
+  }
+    ;
   OLY.prototype.info = function () {
     var _a;
     for (var key in this.datesOLY) {
@@ -299,7 +243,8 @@ var OLY = (function () {
       }
     }
     console.warn("\n\u0421\u0435\u0433\u043E\u0434\u043D\u044F: " + this.theMomentTime.toDateString() + "\n\u0421\u0441\u044B\u043B\u043A\u0430 \u043D\u0430 \u0410\u043F\u0440\u0430\u043A\u043E\u0441: https://aprakos.blogspot.com/" + this.linkToAprakos + "\n\u0421\u0441\u044B\u043B\u043A\u0430 \u043D\u0430 \u043F\u0440\u0430\u0437\u0434\u043D\u0438\u043A: https://aprakos.blogspot.com/" + ((_a = this.linkToHolydays) !== null && _a !== void 0 ? _a : "") + "\n\t\t");
-  };
+  }
+    ;
   OLY.prototype.yearMonthID = function () {
     var otstupka = this.stupka();
     var elemID = this.weeks.current[0] - otstupka;
@@ -355,7 +300,8 @@ var OLY = (function () {
     this.weeks["aprID"] = [aprID, "Апракос-ID"];
     this.weeks["elemID"] = [elemID, "Элемент-ID"];
     return partURL;
-  };
+  }
+    ;
   OLY.prototype.stupka = function () {
     var stupka;
     switch (this.mondayAfterVozdvizgenie()) {
@@ -370,198 +316,51 @@ var OLY = (function () {
         break;
     }
     return stupka;
-  };
+  }
+    ;
   OLY.prototype.stupkaN = function () {
     var stpka = 0;
     if (this.weeks.current[0] >= this.weeks.mif[0]) {
       stpka = this.weeks.stupkaK[0];
-    }
-    else if (this.weeks.current[0] < this.weeks.mif[0]) {
+    } else if (this.weeks.current[0] < this.weeks.mif[0]) {
       stpka = this.weeks.stupkaV[0];
     }
     return stpka;
-  };
+  }
+    ;
   OLY.prototype.stupkaV = function (week) {
     return 0;
-  };
+  }
+    ;
   OLY.prototype.stupkaK = function () {
     return 0;
-  };
+  }
+    ;
   OLY.prototype.holydays_9 = function () {
     var link_to_hld9 = undefined;
     var tmt = this.theMomentTime.getMonth() + "/" + this.theMomentTime.getDate();
     for (var item in this.NINEHOLIDAYS) {
-      var f = this.NINEHOLIDAYS[item].year +
-        "." +
-        this.NINEHOLIDAYS[item].monthRU +
-        "." +
-        this.NINEHOLIDAYS[item].day;
+      var f = this.NINEHOLIDAYS[item].year + "." + this.NINEHOLIDAYS[item].monthRU + "." + this.NINEHOLIDAYS[item].day;
       var date_9 = new Date(f);
       var h9 = date_9.getMonth() + "/" + date_9.getDate();
       if (h9 === tmt) {
-        link_to_hld9 =
-          "/" +
-          this.NINEHOLIDAYS[item].year +
-          "/" +
-          this.NINEHOLIDAYS[item].monthRU +
-          "/" +
-          this.NINEHOLIDAYS[item].day +
-          ".html";
+        link_to_hld9 = "/" + this.NINEHOLIDAYS[item].year + "/" + this.NINEHOLIDAYS[item].monthRU + "/" + this.NINEHOLIDAYS[item].day + ".html";
         return link_to_hld9;
         break;
       }
     }
-<<<<<<< HEAD
-  },
-  true
-);
-
-// if (document.readyState !== 'loading') {
-//    us_clickInterception();
-//  } else {
-//    document.addEventListener('DOMContentLoaded', us_clickInterception);
-//  }
-
-//  function us_clickInterception() {
-//    var links = document.querySelectorAll('a');
-//    Array.prototype.forEach.call(links, function (link) {
-//      link.addEventListener('click', function () {
-//        /* здесь пишем нужное действие */
-//       alert('РАБОТАЕТ ПЕРЕОПРЕДЕЛЕНИЕ…');
-//      });
-//    });
-//  }
-
-reversePack = true;
-
-if (localStorage.ystm === undefined) {
-  setLocalStorage();
-}
-
-var lastSegment = location.pathname.split("/");
-lastSegment = lastSegment[lastSegment.length - 1];
-
-// Установка в хранилище свойств CSS для модального окна
-function setLocalStorage() {
-  localStorage.ystm = JSON.stringify({
-    entries: null,
-    reversePack: true,
-    timerOff: 0,
-    visibility: "visible",
-    lastSegment: lastSegment,
-  });
-}
-// Вызов модального окна первый раз после полной загрузки страницы
-if (
-  (lastSegment == "stvol.html") &
-  (JSON.parse(localStorage.ystm).entries == null)
-) {
-  document.addEventListener(
-    "DOMContentLoaded",
-    () => {
-      firstPreview();
-    },
-    false
-  );
-}
-
-// Показ стартового модального окна
-const closeClick = '<span id="close" class="close" onclick="closeFP00()">+</span>';
-const commentStvol =
-  "<span class='comment-stvol'>В стволе указаны читаемые седмицы с учетом ступок.<br> Подробнее<a class='a-href' href='https://www.aprakos.ru/p/blog-page.html'> здесь</a>.</div>";
-
-function firstPreview() {
-  // instalLocalStorage();
-  str = `
-        <section id="fp-content" class="fp-content">
-        <b>Текущая седмица:</b>
-        <div>по Пасхе&nbsp; <span class="red bold">${sedmicaNorm},</span></div>
-        <div>по Пять&shy;десят&shy;нице <span class="red bold">
-        ${sedmicaNorm - 7}.</span>
-        <div>${lastSegment === "stvol.html" ? commentStvol : ""}</div></div>
-        <div>${lastSegment === "blog-post.html" ? `Отступка <span class="red bold">${stupka}</span> седм.` : ""}</div></div>
-        ${closeClick}
-        </section>
-        `;
-
-  fp = document.getElementById("first-preview").innerHTML = str;
-  document.querySelector("#fp00").classList.add("fp00");
-  document.querySelector("#first-preview").classList.add("fp01");
-  reversePack = false;
-
-  timerOff = setTimeout(() => {
-    closeFP00();
-    alert(
-      "\n Долгое отсутствие увеличивает расстояние разлуки."
-    );
-  }, 3600000);
-  // return { fp, reversePack };
-}
-
-// Закрытие модального окна
-function closeFP00() {
-  localStorage.ystm = JSON.stringify({
-    entries: 1,
-    opasity: 0,
-    visibility: "hidden",
-  });
-  document.querySelector("#fp00").classList.remove("fp00");
-  document.querySelector("#first-preview").classList.remove("fp01");
-  // Removed items
-  document.querySelector("#close").outerHTML = '<!-- Will embed element-->';
-  document.querySelector("#fp-content").outerHTML = '<!-- Will embed element-->';
-  doubleClick700 = "";
-  reversePack = true;
-  clearTimeout(timerOff);
-}
-
-doubleClick700 = "";
-// Прослушивание нажатий клавиатуры
-document.addEventListener("keyup", function (event) {
-  if (event.code == "F2" || event.key == "Shift") {
-    doubleClick700 += event.code + "";
-    setTimeout(() => {
-      doubleClick700 = "";
-    }, 700);
-    console.log(doubleClick700);
-  }
-
-  const state = doubleClick700 == event.code + event.code;
-  console.log(state);
-  if (state && reversePack) {
-    firstPreview();
-  } else if (state && !reversePack) {
-    closeFP00();
-  }
-
-  if (event.code === "Escape") {
-    closeFP00();
-  }
-});
-
-// rrr = document.getElementById('#first-preview');
-// if (rrr) {
-//   rrr.addEventListener('click', () => {
-//     console.log("CLICK-CLACK")
-//   });
-// }
-// else { console.log('=========================='); }
-
-
-
-=======
     return undefined;
-  };
+  }
+    ;
   OLY.prototype.initElementsDOM = function () {
-    var _a, _b;
+    var _a;
     var stvol = document.location.pathname.split('/').pop();
     if (stvol != "stvol.html") {
       return;
     }
-    (_a = document.getElementById('name')) === null || _a === void 0 ? void 0 : _a.children[0].setAttribute('href', (_b = this.linkToHolydays) !== null && _b !== void 0 ? _b : this.linkToAprakos);
+    (_a = document.getElementById('name')) === null || _a === void 0 ? void 0 : _a.children[0].setAttribute('href', this.linkToAprakos);
     var elemsID = {
       cweek: "" + this.weeks.current[0],
-      title50: "\u041F\u043E \u043F\u044F\u0442\u044C\u0434\u0435\u0441\u044F\u0442\u043D\u0438\u0446\u0435",
       week50: "" + (this.weeks.current[0] - 7),
       glass: "Глаc: " + this.glas(+this.weeks.current[0]),
     };
@@ -569,18 +368,17 @@ document.addEventListener("keyup", function (event) {
       if (Object.prototype.hasOwnProperty.call(elemsID, eid)) {
         if (eid === "cweek" || eid === "week50") {
           document.getElementById(eid).innerHTML = "<a href=\"#week" + this.anchorElemID + "\">" + elemsID[eid] + "</a>";
-        }
-        else if (eid == "title50" && Number(elemsID.week50) < 7) {
-          document.getElementById('id50').setAttribute("style", "display:none");
-        }
-        else {
+        } else if (eid == "title50" && Number(elemsID.week50) < 1) {
+          document.getElementById("id50").className += " hidden";
+        } else {
           document.getElementById(eid).innerHTML = elemsID[eid];
         }
       }
     }
     document.getElementById("week" + this.weeks.elemID[0]).className += " colorBlock";
     document.getElementById("weekday" + this.weeks.aprID[0]).className += " seeddayON";
-  };
+  }
+    ;
   OLY.prototype.glas = function (sedmica) {
     var x = sedmica;
     var n = (x % 8) * 0.1;
@@ -596,7 +394,8 @@ document.addEventListener("keyup", function (event) {
         return g.toString();
         break;
     }
-  };
+  }
+    ;
   OLY.prototype.reversePack = function () {
     var _this = this;
     var i = this.stateModalView;
@@ -606,7 +405,8 @@ document.addEventListener("keyup", function (event) {
       return i;
     };
     return reverseP;
-  };
+  }
+    ;
   OLY.prototype.initModalView = function () {
     var _this = this;
     var lastSegment = document.location.pathname.split('/').pop();
@@ -622,7 +422,8 @@ document.addEventListener("keyup", function (event) {
       _this.closeModalView(timerOff);
       alert("\n Долгое отсутствие увеличивает расстояние разлуки.");
     }, 3600000);
-  };
+  }
+    ;
   OLY.prototype.closeModalView = function (timerOff) {
     var _a, _b;
     localStorage.ystm = JSON.stringify({
@@ -637,14 +438,16 @@ document.addEventListener("keyup", function (event) {
     var rpack = this.reversePack();
     rpack();
     clearTimeout(timerOff);
-  };
+  }
+    ;
   OLY.prototype.firstViewModal = function () {
     var aaa = localStorage.ystm;
     if (aaa == null) {
       this.initModalView();
     }
     return {};
-  };
+  }
+    ;
   OLY.prototype.eventKeys = function () {
     var _this = this;
     var oneClickInfo = "";
@@ -657,8 +460,7 @@ document.addEventListener("keyup", function (event) {
       }
       if (oneClickInfo == "F2F2" && _this.stateModalView == false) {
         _this.initModalView();
-      }
-      else if (oneClickInfo == "F2F2" && _this.stateModalView == true) {
+      } else if (oneClickInfo == "F2F2" && _this.stateModalView == true) {
         oneClickInfo = "";
         apr.closeModalView();
       }
@@ -669,14 +471,20 @@ document.addEventListener("keyup", function (event) {
         sessionStorage.removeItem('userDate');
         oneClickInfo = "";
       }
+      console.log(event.code)
+
+      if (event.code == "KeyA" || event.code == "KeyC") {
+        oneClickInfo += event.code + "";
+      }
+      if (oneClickInfo == "KeyAKeyC") {
+        alert("!!!   " + "Выполнена зачистка хранилища…".toUpperCase())
+        localStorage.removeItem('ystm');
+        // sessionStorage.removeItem('userDate');
+        oneClickInfo = "";
+      }
     });
-  };
-  OLY.prototype.deleteUserDateFromSessionStorage = function () {
-    sessionStorage.removeItem('userDate');
-    location.replace(document.documentURI);
-  };
+  }
+    ;
   return OLY;
 }());
-var apr = new OLY();
-var NAMELIST;
-var PROBLEMS;
+var apr = new OLY([]);
