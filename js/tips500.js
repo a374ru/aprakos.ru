3de9e76a1db888d7c77c6ef0f859c54c5d4e5244
Oleg <82386871+a374ru@users.noreply.github.com>
2023-07-13 11:12:27 +0300

    555 ••• 2023

---
+++ js/tips500.js
@@ -0,0 +1,43 @@
+﻿(function ($) {
+	$(function () {
+
+		$('span.ttt').each(function () {
+			var el = $(this);
+			var title = el.attr('title');
+
+
+			if ($(window).width() <= '500') {
+				// здесь код вычисления границ и замены стиля элемента для мобильного вида
+				el.attr('title', '').append('<div class="ttt_div">' + title + '</div>');
+
+			} else {
+				el.attr('title', '').append('<div>' + title + '</div>');
+			}
+
+			if (title != "") {
+				var width = el.find('div').width(); // получение ширины
+				var height = el.find('div').height(); // получение высоты
+				var left = none;
+				el.hover(
+					function () {
+
+						el.find('div')
+							.clearQueue()
+							.delay(200)
+							.animate({ width: width + 20, height: height + 20 }, 50).show(150)
+							.animate({ width: width, height: height }, 100);
+
+					},
+					function () {
+						el.find('div')
+							.animate({ width: width + 20, height: height + 20 }, 100)
+							.animate({ width: 'hide', height: 'hide' }, 150);
+					}
+				).mouseleave(function () {
+					if (el.children().is(':hidden')) el.find('div').clearQueue();
+				});
+			}
+		})
+
+	})
+})(jQuery)
