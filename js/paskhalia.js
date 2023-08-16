227881c5ffa95c9f2c1cade55056a38d697fe106
Oleg <82386871+a374ru@users.noreply.github.com>
2023-08-16 13:51:45 +0300

    555 ••• 2023 YSTM-MAC

---
+++ js/paskhalia.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ClassModelTest = void 0;
+class ClassModelTest {
+    read() {
+        console.log("---OK!-- javascript module tessting ---OK!---");
+    }
+}
+exports.ClassModelTest = ClassModelTest;
