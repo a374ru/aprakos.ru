1f2df14e92ec4c544b0947f1a033446b5f91c248
Oleg <82386871+a374ru@users.noreply.github.com>
2024-02-21 12:15:31 +0300

    _Auto commit ••• 2024 MacBook

--- js/csyear.js
+++ js/csyear.js
@@ -1,56 +1,72 @@
-"use strict";
-/**
- *
- *  Конвертирование григорианского года
- *  от 1000-го и далее в Церковный славянский (цсл).
- *
- *  Требуется подключенный цсл. шрифт.
- *
- */
-let currentYearGRG = new Date().getFullYear();
-const massYearsCSL = {
-	tschi: "¤",
-	cifri: ['', 'а', "в", 'г', 'д', 'є', 's', 'з', 'и', 'f'],
-	desyatki: ['', 'i', 'к', 'л', 'м', 'н', '…', 'o', 'п', 'ч'],
-	sotni: ['', 'р', 'с', 'т', '¢', 'f', 'х', 'p', 't', 'ц']
-};
-function currentYearCSl() {
-	let yearInsert = massYearsCSL.tschi;
-	let tsch = massYearsCSL.tschi;
-	let yearString = String(currentYearGRG);
-	if (yearString.length == 3) {
-		yearString = "0" + yearString;
-		tsch = "";
-	}
-	// Считываем цифру тысяч
-	let tscha = Number(yearString.slice(0, 1));
-	// Считываем цифру сотен
-	let sotnya = Number(yearString.slice(1, 2));
-	// Считываем цифру десятков
-	let desyatok = Number(yearString.slice(2, 3));
-	// Считываем цифру единиц
-	let edinica = Number(yearString.slice(-1));
-	if (sotnya != 0) {
-		// реализация сотен
-		yearInsert += massYearsCSL.cifri[tscha] + massYearsCSL.sotni[sotnya]
-			+ massYearsCSL.desyatki[desyatok] + "7" + massYearsCSL.cifri[edinica];
-	}
-	else {
-		// реализация десятков
-		yearInsert += massYearsCSL.cifri[tscha] + massYearsCSL.desyatki[desyatok]
-			+ "7" + massYearsCSL.cifri[edinica];
-	}
-	// для годов второго десятка (10…19)
-	if (Number(yearString.slice(2, 4)) < 20) {
-		yearInsert = tsch + massYearsCSL.cifri[tscha]
-			+ massYearsCSL.sotni[sotnya]
-			+ massYearsCSL.cifri[edinica] + "7" + massYearsCSL.desyatki[desyatok];
-	}
-	return yearInsert;
+
+if (window.innerWidth < 525) {
+    b = document.body.setAttribute('style', 'background: radial-gradient(circle, rgb(242, 244, 255) 30%, rgb(255, 240, 243) 70%)');
 }
 
-var idElementDate = document.getElementById('ystm-date')
-if (idElementDate) {
-	idElementDate.innerHTML = currentYearCSl();
-	idElementDate.setAttribute('title', `${currentYearGRG} год`);
-}
+
+const arr = [
+    "Январь",
+    "Февраль",
+    "Март",
+    "Апрель",
+    "Май",
+    "Июнь",
+    "Июль",
+    "Август",
+    "Сентябрь",
+    "Октябрь",
+    "Ноябрь",
+    "Декабрь",
+];
+
+window.addEventListener("load", function () {
+    li = document.querySelectorAll("li");
+    moment = new Date();
+    numberMontf = moment.getMonth();
+    curentYear = moment.getFullYear();
+    spanElement = `<br><a target='_blank' href='https://aprakos.a374.ru/currentday/APRAKOS/PASKHA/XB.html' class='badge-link'>Грядущая Пасха</a>`
+
+    //=-=-=-=-=-=-=-=-=-=-=-=-=-
+    li.forEach((node) => {
+        let node3 = node.textContent.match(curentYear - 3);
+        if (node3) {
+            //anchor for scroll
+            node.setAttribute("id", "easter");
+        }
+
+        yearElemet = node.textContent.match(curentYear);
+
+        if (yearElemet != null) {
+dateElement = Number(node.textContent.match(/[\d]{1,2}$/)[0].split(",")[0]);
+            for (var el of arr) {
+                if (node.textContent.search(el) > -1) {
+                    indexMonthElement = arr.findIndex((i) => i == el);
+                }
+            }
+            easterElement = new Date(
+                `${yearElemet}/${indexMonthElement+0}/${dateElement}`
+            );
+            //=-=-=-=-=-=-=-=-=-=-=-=-=-
+            if (moment.getTime() < easterElement.getTime()) {
+                node.setAttribute("class", "last-easter");
+                node.setAttribute("class", "easter pulse");
+                node.innerHTML += spanElement
+
+            } else {
+                node.setAttribute("class", "last-easter");
+            }
+        }
+
+        node2 = node.textContent.match(curentYear + 1);
+        if (node2 && moment.getTime() > easterElement.getTime()) {
+            node.setAttribute("class", "easter pulse");
+            formatText = node.textContent.replace(/[\(\\)]{1}\s{1}/gi, '');
+            node.innerHTML = formatText + spanElement;
+        }
+    });
+    // el = document.querySelector("#easter");
+    // el.scrollIntoView();
+    document.location.replace("#easter");
+});
+
+document.querySelector(".copyright-year").innerHTML = new Date().getFullYear();
