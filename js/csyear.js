8d3ccc0f47d6360fb963f72a4ce1fdc6fbd2ace0
Oleg Borodkin <82386871+a374ru@users.noreply.github.com>
2025-04-29 20:19:04 +0300

    Auto commit ••• 2025

--- js/csyear.js
+++ js/csyear.js
@@ -1,11 +1,11 @@
 // скрипт предназначен исключительно для домена https://easter.a374.ru/
 
-try{
+try {
     if (window.innerWidth < 525) {
         b = document.body.setAttribute('style', 'background: radial-gradient(circle, rgb(242, 244, 255) 30%, rgb(255, 240, 243) 70%)');
     }
-    
-    
+
+
     const arr = [
         "Январь",
         "Февраль",
@@ -20,14 +20,14 @@ try{
         "Ноябрь",
         "Декабрь",
     ];
-    
+
     window.addEventListener("load", function () {
         li = document.querySelectorAll("li");
         moment = new Date();
         numberMontf = moment.getMonth();
         curentYear = moment.getFullYear();
         spanElement = `<br><a target='_blank' href='https://aprakos.blogspot.com/p/zz-c-apr.html' class='badge-link'>Грядущая Пасха</a>`
-        
+
         //=-=-=-=-=-=-=-=-=-=-=-=-=-
         li.forEach((node) => {
             let node3 = node.textContent.match(curentYear - 3);
@@ -35,9 +35,9 @@ try{
                 //anchor for scroll
                 node.setAttribute("id", "easter");
             }
-            
+
             yearElemet = node.textContent.match(curentYear);
-            
+
             if (yearElemet != null) {
                 dateElement = Number(node.textContent.match(/[\d]{1,2}$/)[0].split(",")[0]);
                 for (var el of arr) {
@@ -46,19 +46,19 @@ try{
                     }
                 }
                 easterElement = new Date(
-                        `${yearElemet}/${indexMonthElement+0}/${dateElement}`
-                        );
+                    `${yearElemet}/${indexMonthElement + 0}/${dateElement}`
+                );
                 //=-=-=-=-=-=-=-=-=-=-=-=-=-
                 if (moment.getTime() < easterElement.getTime()) {
                     node.setAttribute("class", "last-easter");
                     node.setAttribute("class", "easter pulse");
                     node.innerHTML += spanElement
-                    
+
                 } else {
                     node.setAttribute("class", "last-easter");
                 }
             }
-            
+
             node2 = node.textContent.match(curentYear + 1);
             if (node2 && moment.getTime() > easterElement.getTime()) {
                 node.setAttribute("class", "easter pulse");
@@ -70,12 +70,12 @@ try{
         // el.scrollIntoView();
         document.location.replace("#easter");
     });
-    
+
     document.querySelector(".copyright-year").innerHTML = new Date().getFullYear();
 }
 
-catch(err){
-    
+catch (err) {
+
     console.log("Проверте наличие класса <.copyright-year>.", err.message);
-    
+
 }
